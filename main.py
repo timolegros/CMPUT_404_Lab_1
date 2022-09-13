288576b86400c66ac085a94be0984cd1371e123c
Timothee Legros <timothee@ualberta.ca>
2022-09-12 19:18:12 -0600

    print source code

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import requests
+from bs4 import BeautifulSoup
 
 print(requests.__version__)
 
 r = requests.get("https://www.google.com")
 
-r = requests.get("https://github.com/timolegros/CMPUT_404_Lab_1/blob/main/main.py")
+r2 = requests.get("https://github.com/timolegros/CMPUT_404_Lab_1/blob/main/main.py")
+soup = BeautifulSoup(r2.text, 'html.parser')
+
+table = soup.select('table[data-tagsearch-path="main.py"]')
+items = table[0].select('td[class="blob-code blob-code-inner js-file-line"]')
+
+print("\n\nSource code:\n")
+for item in items:
+    print(item.text)
+
+
